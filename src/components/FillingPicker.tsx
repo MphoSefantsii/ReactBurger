3b36cd40711328418cdd442c3cfab8f794ff04cb
Mpho Sefantsi <mpho.sefansi@africacode.academy>
2024-07-22 13:29:30 +0200

    Correcting auto generating id

--- src/components/FillingPicker.tsx
+++ src/components/FillingPicker.tsx
@@ -4,65 +4,71 @@ import tomato from '../images/tomato.jpg';
 import lettuce from '../images/lettuce.jpg';
 import meat from '../images/meat.jpg';
 
-interface Filling {
+interface FillingType {
     color: string;
     type: string;
     id: string;
 }
 
 interface Props {
-    setFilling: (filling: Filling[]) => void;
+    setFilling: React.Dispatch<React.SetStateAction<FillingType[]>>;
 }
 
 const FillingPicker: React.FC<Props> = ({ setFilling }) => {
     const addLayer = (fillingType: string) => {
         let layerBackgroundColor = "";
 
-        if (fillingType === "meat") {
-            layerBackgroundColor = "brown";
-        } else if (fillingType === "lettuce") {
-            layerBackgroundColor = "green";
-        } else if (fillingType === "tomato") {
-            layerBackgroundColor = "red";
+        switch (fillingType) {
+            case "meat":
+                layerBackgroundColor = "brown";
+                break;
+            case "lettuce":
+                layerBackgroundColor = "green";
+                break;
+            case "tomato":
+                layerBackgroundColor = "red";
+                break;
+            default:
+                layerBackgroundColor = "transparent";
         }
 
-        const newLayer: Filling = {
+        const newLayer: FillingType = {
             color: layerBackgroundColor,
             type: fillingType,
             id: uuidv4()
         };
 
-        setFilling((prevFilling: Filling[]) => [newLayer, ...prevFilling]);
+        setFilling((prevFilling) => [newLayer, ...prevFilling]);
     };
+
     return (
         <div className="filling-picker">
             <p className="header">
                 <b>Click to add in your burger</b><br/>
                 <i>Enjoy best meals</i>
             </p>
-                <button
+            <button
                 className="tomato"
                 type="button"
                 onClick={() => addLayer("tomato")}
             >
-                <img src={tomato}/>
+                <img src={tomato} alt="Tomato" />
             </button>
             <button
                 className="lettuce"
                 type="button"
                 onClick={() => addLayer("lettuce")}
             >
-                <img src={lettuce}/>
+                <img src={lettuce} alt="Lettuce" />
             </button>
             <button
                 className="meat"
                 type="button"
                 onClick={() => addLayer("meat")}
             >
-                <img src={meat}/>
+                <img src={meat} alt="Meat" />
             </button>
-
-    </div>
+        </div>
     );
 };
 
